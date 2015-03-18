b7ac23e3497f9cbed871d7b82a1d8481380df737
Tim Moreton <tmoreton89@gmail.com>
2015-03-18 18:43:09 -0400

    removed mongodb

--- server/db/index.js
+++ server/db/index.js
@@ -3,24 +3,24 @@ var Q = require('q');
 var path = require('path');
 var chalk = require('chalk');
 
-var DATABASE_URI = require(path.join(__dirname, '../env')).DATABASE_URI;
+// var DATABASE_URI = require(path.join(__dirname, '../env')).DATABASE_URI;
 
-var mongoose = require('mongoose');
-var db = mongoose.connect(process.env['MONGOLAB_URI'] || DATABASE_URI).connection;
+// var mongoose = require('mongoose');
+// var db = mongoose.connect(process.env['MONGOLAB_URI'] || DATABASE_URI).connection;
 
 // Require our models -- these should register the model into mongoose
 // so the rest of the application can simply call mongoose.model('User')
 // anywhere the User model needs to be used.
-require('./models/user');
+// require('./models/user');
 
-var startDbPromise = new Q(function (resolve, reject) {
-    db.on('open', resolve);
-    db.on('error', reject);
-});
+// var startDbPromise = new Q(function (resolve, reject) {
+//     db.on('open', resolve);
+//     db.on('error', reject);
+// });
 
-console.log(chalk.yellow('Opening connection to MongoDB . . .'));
-startDbPromise.then(function () {
-    console.log(chalk.green('MongoDB connection opened!'));
-});
+// console.log(chalk.yellow('Opening connection to MongoDB . . .'));
+// startDbPromise.then(function () {
+//     console.log(chalk.green('MongoDB connection opened!'));
+// });
 
-module.exports = startDbPromise;
+// module.exports = startDbPromise;
